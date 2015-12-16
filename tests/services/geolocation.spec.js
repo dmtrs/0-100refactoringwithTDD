5fd1f5d0d89c22915b77c98583dd4674f0da0642
dmtrs <tydeas.dr@gmail.com>
2015-12-16 02:21:37 +0200

    fix(test) Add first test

--- tests/services/geolocation.spec.js
+++ tests/services/geolocation.spec.js
@@ -1,5 +1,15 @@
 var test = require('tape');
 var Geolocation = require('../../services/geolocation');
 
-//test('Geolocation.getCountry()', function(t) {
-//});
+test('Geolocation.getCountry() should return iso country code', function(t) {
+  t.plan(1);
+
+  var ip = '8.8.8.8';
+  var expected = 'USA';
+
+  var geo = new Geolocation();
+  geo.getCountry(ip, function(err, actual) {
+    t.same(actual, expected);
+  });
+
+});
